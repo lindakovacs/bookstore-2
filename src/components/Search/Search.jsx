2181ef84059797c645a8c44a197a070c3da3bf0e
Simi Asaro <sasaro@advancelocal.com>
2018-11-11 14:57:50 -0500

    results in state but not displaying

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,37 +1,78 @@
-import React from 'react'
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 
-function Search(props) {
-    console.log(props.match.params);
-    const handleChange = (e) => {
-        console.log(e.target.value);
-        props.match.params.inputQuery =  e.target.value;
-    }
-    const getData = () => {
-        let url = 'http://localhost:7000/books/search/' + props.match.params.inputQuery;
-        axios.get(url)
-            .then(response => {
-                return response;
-            })
-            .catch(error => console.log(error) );        
-    }
-    console.log(getData);
-    return (
-        <div>
-            <form onSubmit={e => getData(e.target.value)}>
-                <input onChange={handleChange} />
-                <button type="submit">Search...</button>
-            </form>
-            {props.match.params.getData > 0 && props.match.params.getData.map((item, index) => {
-                return (
-                    <ul>
-                        <li>{item}</li>
-                    </ul>
-                );
-            })
+class Search extends Component {
+    state = {
+        queryInput: null,
+        results: [],
+        isError: false,
+        loading: false
+    };
+
+    search = (e) => {
+        e.preventDefault();
+        this.setState({ 
+            queryInput: e.target.value
+        });
+    };
+
+    getBooks = () => {
+        console.log("getBooks: ", this.state);
+        this.setState({
+            loading: true
+        });
+        axios
+			.get(`http://localhost:7000/books/search/apple${this.state.queryInput}`)
+			.then(response => {
+				console.log("axios: ", response.data);
+				this.setState({ 
+                    results: response.data,
+                    loading: false 
+                });
+			})
+			.catch(error => {
+				this.setState({ isError: true });
+			});
+    };
+
+    componentDidMount() {
+        console.log('componentDidMount');
+        this.getBooks();
+    };
+
+    render() {
+        if (this.state.results) {
+            console.log('I HAVE DATA!!!: ', this.state.results);
         }
-        </div>
-    )
+        const { results } = this.state;
+        return (
+            <div className="BooksSearch">
+                <div className="Search">
+                    <input
+                        type="search"
+                        placeholder="Search..."
+                        aria-label="Search"
+                        className="search"
+                        onChange={e => this.search(e)}
+                    />
+                    <button type="submit" onClick={this.getBooks}>Search...</button>
+                </div>
+                <div className="books-results">
+                    {results.books}
+                    {/* {results && results.map((book, index) => {
+                        console.log(book);
+                        const key = 'book-' + index;
+                        const title = book.title;
+                        return (
+                            <div className="book" key={key}>
+                                {title}: {book}
+                            </div>
+                        );
+                    })} */}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Search;
