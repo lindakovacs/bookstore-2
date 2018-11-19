360a439a8fddfa773bd18380d6a7d4dc6c922b8c
Simi Asaro <sasaro@advancelocal.com>
2018-11-19 13:35:51 -0500

    display dropdown got from internet

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,23 +1,20 @@
 import React, { Component } from 'react'
 import axios from "axios";
-import { Link } from 'react-router-dom'
-import { Media, DropdownButton, MenuItem, ButtonToolbar } from 'react-bootstrap';
-
-const TYPES = ['Want to Read', 'Currently Reading', 'Read'];
+import { Media } from 'react-bootstrap';
+import DisplayContainer from './DisplayContainer';
 
 class BookDetails extends Component {
     state = {
         bookId: "",
         book: {},
-        bookshelfPick: 0,
+        displayValue: ('None'),
         isError: false,
-        bookshelfTypesIndex: 0
+        myBooks: {'Want To Read': [], 'Currently Reading': [], 'Read': []}
     };
     componentDidMount() {
         console.log("didmount: " , this.props.match.params.bookId);
         this.getBookDetails();
-    }
-
+    };
     getBookDetails = () => {
     if (this.props.match.url) {
         this.setState(
@@ -38,44 +35,10 @@ class BookDetails extends Component {
                     }
                 )
         }
-    }
-
-// const bookId = props.match.params.bookId;
-    // const url = props.match.url;
-    // function getBookDetails() {
-    //     return axios
-    //         .get(`http://localhost:7000/${url}`)
-    //         .then(response => {
-    //             return response.data;
-    //         })
-    //         .catch(error => {
-    //             console.log(error);
-    //         });
-    // }
-    // let details = getBookDetails();
-    // console.log(details);
-    // //const image = (details.imageLinks ? details.imageLinks.medium : details.imageLinks.thumbnail);
-    // const authors = details.authors;
-    // const title = details.title;
-    // const subtitle = (details.subtitle ? details.subtitle : "");
-    // const description = details.description
+    };
     render() {
         console.log('bookdetails: ', this.state);
         const { book } = this.state;
-        const buttonsInstance = (
-            <ButtonToolbar>
-                <DropdownButton title="None" id="dropdown-size-medium">
-                    <MenuItem eventKey="1">Want To Read</MenuItem>
-                    <MenuItem eventKey="2">Currently Reading</MenuItem>
-                    <MenuItem eventKey="3">Read</MenuItem>
-                    {/* {TYPES.map((type, index) => {
-                        return (
-                            <MenuItem eventKey={index}>{type}</MenuItem>
-                        )})
-                    } */}
-                </DropdownButton>
-            </ButtonToolbar> 
-        );
         return <div>
                 <Media>
                     <Media.Left align="top">
@@ -97,15 +60,10 @@ class BookDetails extends Component {
                     </Media.Body>
                 </Media>
                 <p>
-                    <h3>MyBookshelf Status</h3>
-                    <ButtonToolbar>
-                        <DropdownButton title="None" key={0} id="dropdown-basic" onChange={e => this.pickBookStatus(e.target.value)}>
-                        {TYPES.map((type, index) => {
-                            console.log(type, index);
-                            return <MenuItem eventKey={index} onSelect={eventKey => "active"}>{type}</MenuItem>
-                        })}
-                        </DropdownButton>
-                    </ButtonToolbar>
+                    <h3>Shelf</h3>
+                    <div>
+                        <DisplayContainer/>
+                    </div>
                 </p>
             </div>;
     }
